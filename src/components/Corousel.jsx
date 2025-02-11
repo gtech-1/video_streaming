64977c03a34cd2b8899f5264ad629fceeb91bb94
gtech-1 <ganeshmsavadattimath@gmail.com>
2025-02-11 20:46:00 +0530

    auth and sidebar project setup

--- src/components/Corousel.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Corousel() {
-  return (
-    <div>Corousel</div>
-  )
-}
-
-export default Corousel
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+function Home() {
+  return (
+    <div>
+        <Sidebar/>
+        {/* dashboard */}
+    </div>
+  )
+}
+
+export default Home
