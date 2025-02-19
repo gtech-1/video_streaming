55c39bc7d6ea1ed6aff751b38bbbd10955c7f8a2
DeepakBaligar <deepakbaligar-83@users.noreply.github.com>
2025-02-19 19:28:31 +0530

    intial changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import './App.css'
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import SignIn from "./components/SignIn";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-  <>
-   <h1 className="text-3xl font-bold underline text-center">
-      Hello world!
-    </h1>
-  </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/login" element={ <Login />} />
+        <Route path="/dashboard" element={ <Dashboard />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
+const CLIENT_ID = "1098499974317-mfi4v5n1hujtcguprinrft3ojqo7tkj4.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+     <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
