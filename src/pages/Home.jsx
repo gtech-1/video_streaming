828d2171f66cf9dff6f38b72af0baf11b22bc34f
Saismarth Udikeri <saisamarthudikeri@gmail.com>
2025-03-06 12:28:47 +0530

    latest

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,17 +10,25 @@ const Home = () => {
     setIsSidebarOpen((prevState) => !prevState);
   };
 
-
   return (
     <div className="bg-gray-100 min-h-screen pt-16 flex">
       <Navbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
       <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <div className={`flex-1 transition-all ${isSidebarOpen ? "ml-64" : "ml-0"}`}>
+
+      {/* Dashboard moves only on desktop, stays fixed on mobile */}
+      <div className={`flex-1 transition-all duration-300 ${isSidebarOpen ? "lg:ml-64" : "ml-0"}`}>
         <Dashboard />
       </div>
+
+      {/* Background overlay when sidebar is open on mobile */}
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 lg:hidden"
+          onClick={toggleSidebar}
+        ></div>
+      )}
     </div>
   );
 };
 
 export default Home;
-
